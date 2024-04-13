0058348f62f822006dd32bfb2cad8551a8b3038f
neutr0n420 <aryan.bramhane@ortigan.in>
2024-04-14 01:18:34 +0530

    added greeter message on boot

--- lua/aryan/core/keymaps.lua
+++ lua/aryan/core/keymaps.lua
@@ -15,6 +15,7 @@ keymap.set("n", "<leader>sh", "<C-w>s", {desc="Split window horizantally"}) -- s
 keymap.set("n", "<leader>se", "<C-w>=", {desc="Make splits equal size"}) --  make split windows equal 
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc="close current split"}) -- close current split window 
 
+-- Tmux and 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", {desc="Open new tab"}) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", {desc=" new tab"}) -- close new tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", {desc="Go to next tab"}) -- go to next tab
@@ -26,3 +27,9 @@ keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file e
 keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
 keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
 keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+
+ -- Telescope Keybindings
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
